b73ebb2002e368be23ac346c0b537d01d6e65ea6
Joe Rocca <jprocca813@gmail.com>
2017-04-11 10:14:29 -0400

    Initial commit

---
+++ app/src/main/java/com/joerocca/tictactoe/ControlFragment.java
@@ -0,0 +1,46 @@
+//package com.joerocca.tictactoe;
+//
+//import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+//}
+package com.joerocca.tictactoe;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ControlFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView =
+                inflater.inflate(R.layout.fragment_control, container, false);
+        View main = rootView.findViewById(R.id.button_main);
+        View restart = rootView.findViewById(R.id.button_restart);
+
+        main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().finish();
+            }
+        });
+        restart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((GameActivity) getActivity()).restartGame();
+            }
+        });
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/joerocca/tictactoe/MainActivity.java
@@ -0,0 +1,26 @@
+//package com.joerocca.tictactoe;
+//
+//import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+//}
+package com.joerocca.tictactoe;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
